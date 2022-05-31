8b7bd96ba88aa09607fe28b22186ebb6c40e7bc4
Yash Bijesh Patel <106303528+yashp2003@users.noreply.github.com>
2022-05-31 07:15:55 +0530

    Project no.3
    31/05/2022. Program to perform all arithematic operations with 2 user input values.

---
+++ pr_003_Adding_2nos.py
@@ -0,0 +1,13 @@
+# Adding 2 random values
+a = 555
+b = 438
+print("The addition of a + b is ", a + b)
+# All Arithematic operations with 2 user input values
+a = int(input("Enter the first number: "))
+b = int(input("Enter the second number: "))
+print("The sum of a + b is : ", a+b) 
+print("The Sub of a - b is : ", a-b)
+print("The mul of a * b is : ", a*b)
+print("The div of a / b is : ", a/b)
+# Note python consider user input values as string, so it's very imp to typecast it to perform any mathematical operations with the user input values.
+
