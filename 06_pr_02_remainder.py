be527705dd6982b4d82803858393f5ce442e0f4a
Yash Bijesh Patel <106303528+yashp2003@users.noreply.github.com>
2022-05-31 07:36:08 +0530

    Add files via upload

---
+++ 06_pr_02_remainder.py
@@ -0,0 +1,3 @@
+number = int(input("Enter the number : "))
+print("The remainder when the entered value is divisible by 2 is", number%2)
+print("The remainder when the entered value is divisible by 3 is", number%3)
